ba3395b2ed74bcf1995f71e93dfbd1027113e255
Kadi-Yoda <47210367+Kadi-Yoda@users.noreply.github.com>
2019-03-29 00:32:28 +0100

    Add files via upload

---
+++ projet NaN JS/script.js
@@ -0,0 +1,55 @@
+function changeImage(){
+
+	var img=document.getElementById('inverse');
+	img.src="image/orange.jpg";
+};
+function retourImage(){
+		var img=document.getElementById('inverse');
+		img.src="image/orange2.jpg";
+};
+function changeImage2(){
+
+	var img=document.getElementById('banane');
+	img.src="image/banane.jpg";
+};
+function retourImage2(){
+		var img=document.getElementById('banane');
+		img.src="image/banane2.jpg";
+};
+function changeImage3(){
+
+	var img=document.getElementById('poire');
+	img.src="image/poire2.jpg";
+};
+function retourImage3(){
+		var img=document.getElementById('poire');
+		img.src="image/poire.jpg";
+};
+function changeImage4(){
+
+	var img=document.getElementById('raisin');
+	img.src="image/raisin2.jpg";
+};
+function retourImage4(){
+		var img=document.getElementById('raisin');
+		img.src="image/raisin.jpg";
+};
+function changeImage5(){
+
+	var img=document.getElementById('mangue');
+	img.src="image/mangue.jpg";
+};
+function retourImage5(){
+		var img=document.getElementById('mangue');
+		img.src="image/mangue2.jpg";
+};
+
+
+
+
+
+//bare de navigation//
+function color(){
+	var x=document.getElementById('couleur');
+	x.style.backgroundColor='#FF00FF';
+}
